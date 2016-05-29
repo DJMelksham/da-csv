bb58e1d901ac265c585113cbbd3b1added5cf527
Damien John Melksham <Damien.Melksham@gmail.com>
2016-05-29 22:52:51 +1000

    Wrote a very basic csv finite state machine after writing it out on a whiteboard

--- da-csv.lisp
+++ da-csv.lisp
@@ -113,122 +113,82 @@
     :accessor confirmed-irregular
     :documentation "If the delimited file is believed to be irregular in nature, that is to say, that each row does not contain a similar number of variables, we can set this slot to T to indicate that fact.")))
 
-(defun make-parse-state ()
-  "Creates an array representing the state of a csv parsing operation
-   Values are binary, and Indexes are equivalent to
-   0: in field
-   1: in row
-   2: saw-quote
-   4: file-position"
+;;; The csv finite state machine is the base level parser of csv files.
+;;; It doesn't do anything except take in a character and transition
+;;; states. The decision of what to do when states transition is
+;;; left to a higher level function.
+
+;;; Believe it or not, there are 6 possible parsing states
+;;; the csv parser can be in. They are enumerated 0 through 5.
+;;;
+;;; They are:
+;;;
+;;; 0: Beginning of row state: beginning of a file, or the state entered after
+;;;    seeing a newline at the end of the row.
+;;; 1: Basic character: saw a vanilla character and we weren't in a quote.
+;;;    Expected course of action if we're monitoring this is to accept that
+;;;    character as valid output for a field.
+;;; 2: Between fields: seen in the case of seeing a significant delimiter
+;;;    character.  Don't want to output it, but its a signal that a field has ended.
+;;; 3: Quote at beginning of field: What the name says.  We can transition to state
+;;;    3 only from state 0 or 2.
+;;; 4: Non-quote character seen while in quote field.  Very similar behaviour to state 1
+;;;    except even more liberal - expected course of action is to accept that character
+;;;    as a valid character for a field.
+;;; 5: Seeing a quote within a quote field:  This means either that we're at the end
+;;;    of the field, or that the next character will be a quote.
+;;;
+;;; The possible transitions between the states are labelled N, D, Q, and E.
+;;;
+;;;    N - See newline character
+;;;    D - See delimiter character
+;;;    Q - See quote character
+;;;    E - Everything else
+;;;
+;;; The mapping of states and transitions is thus as follows:
+;;; If a transition is not included for a state, it is because
+;;; this is an undefined/erroneous transition for that particular
+;;; state if we are dealing with a parsable csv file.
+;;;
+;;; 0: N -> 0, E -> 1, D -> 2, Q -> 3, 
+;;; 1: N -> 0, E -> 1, D -> 2
+;;; 2: N -> 0, E -> 1, D -> 2, Q -> 3
+;;; 3: N -> 4, E -> 4, D -> 4, Q -> 5
+;;; 4: N -> 4, E -> 4, D -> 4, Q -> 5
+;;; 5: N -> 0,         D -> 2, Q -> 4
+;;;
+
+(declaim inline csv-fs-machine)
+
+(defun csv-fs-machine (state seen delimiter newline quote)
+  "CSV Finite State Machine - reads chars and returns transitioned states"
+  (declare (character seen delimiter newline quote)
+	   (fixnum state)
+	   (optimize (speed 3)))
   
-  (make-array 4 :element-type 'fixnum))
-
-;;twiddle a array element between the two binary values of 1 and 0
-(defun aref-twiddle (state-array index)
-  (setf (aref array index)
-	(if (= (aref array index) 0)
-	    1
-	    0)))
-
-(defun field-twiddle (state-array)
-  (aref-twiddle state-array 0))
-
-(defun row-twiddle (state-array)
-  (aref-twiddle state-array 1))
-
-(defun saw-quote-twiddle (state-array)
-  (aref-twiddle state-array 2))
-
-(defun incf-file-pos (state-array)
-  (incf (aref state-array 3)))
-
-(defun in-field? (state-array)
- (if  (= (aref state-array 0) 1)  
-      T
-      NIL))
-
-(defun in-row? (state-array)
-  (if (= (aref state-array 1) 1)
-      T
-      NIL))
-
-(defun saw-quote? (state-array)
-  (if (= (aref state-array 2) 1)
-      T
-      NIL))
-
-;;Set an array element to 0 
-
-(defmacro aref-0 (array index)
-  `(setf (aref ,array ,index) 0))
-    
-(defun view-char-update-state (state-array char &optional
-					(separator *delimiter*)
-					(quote-char *quote-char*)
-					(newline-char *newline-char*))
-
-  ;; If we are in a state where we're in nothing and have seen nothing
-  
-  (if (and (null (in-field? state-array))
-	   (null (in-row? state-array))
-	   (null (saw-quote? state-array)))
-  
-            ;; Not in anything, seen nothing - see quote
-      (cond ((eql char quote-char)
-	     ())
-	    
-
-	    ;; Not in anything, seen nothing - see newline-char	     
-	    ;; I don't think that one should happen, but lets take care of it anyway.
-	    ((eql char newline-char)
-	     ())
-
-	    ;; Not in anything, seen nothing - see separator
-	    ((eql char separator)
-	     ())
-
-	    ;; Not in anything, seen nothing - see non-special-character
-	    (t
-	     ())))
-
-  ;; If we are in a state where we're in a field
-  ;; (and must implicitly therefore also be in a row)
-  ;; and have not seen a quote
-
-  (if (and (in-field? state-array)
-	   (null (saw-quote? state-array)))
-      (cond (()())))
-  
-  ;; If we are in a state where we're in a field
-  ;; and we have seen a quote
-
-  (if (and (in-field? state-array)
-	   (saw-quote? state-array))
-      (cond (()())))
-
-  ;; If we are in a state where we're not in a field, but we are in a row
-  ;; and we haven't seen a quote.
-
-  (if (and (null (in-field? state-array))
-	   (in-row? state-array)
-	   (null (saw-quote? state-array)))
-      (cond (()())))
-
-  
-  ;; If we are in a state where we're not in a field, but we are in a row
-  ;; and we have seen a quote.
-
-  (if (and (null (in-field? state-array))
-	   (in-row? state-array)
-	   (saw-quote? state-array))
-      (cond (()())))
-
-		     
-  ;;We've just seen a character, so we should increment the state
-  ;;of where we are in a delimited file/
-  (incf (aref state-array 3))
-  
-
-					
-  )
+  (cond ((eql state 1)
+	 (cond ((char= seen delimiter) 2)
+	       ((char= seen newline) 0)
+	       (t 1)))
+	((eql state 2)
+	 (cond ((char= seen delimiter) 2)
+	       ((char= seen newline) 0)
+	       ((char= seen quote) 3)
+	       (t 1)))
+	((eql state 0)
+	 (cond ((char= seen delimiter) 2)
+	       ((char= seen newline) 0)
+	       ((char= seen quote) 3)
+	       (t 1)))
+	((eql state 3)
+	 (cond ((char= seen quote) 5)
+	       (t 4)))
+	((eql state 4)
+	 (cond ((char= seen quote) 5)
+	       (t 4)))
+	;;If we get to here, we must be in state 5, so don't have to check for it.
+	(t
+	 (cond ((char= seen quote) 4)
+	       ((char= seen delimiter) 2)
+	       (t 0))))) ; must be a newline, because other characters would be invalid.
 
